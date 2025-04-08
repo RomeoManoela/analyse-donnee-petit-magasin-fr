db1e3e193d85657b5efa3823986b2a0710018c8f
romeomanoela <romeomanoela18@gmail.com>
2025-04-08 07:25:21 +0300

    update, adding streamlit

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import numpy as np
 import pandas as pd
 
-data: pd.DataFrame = pd.read_csv('data/ventes.csv')
+import streamlit as st
 
-# convertir la date en datetime
-data['Date']: pd.Series = pd.to_datetime(data['Date'])
+st.title("Visualisation des données")
+
+data: pd.DataFrame = pd.read_csv("data/ventes.csv")
 
 # calculer une nouvelle colonne
-data['Montant']: pd.Series = np.round(data['Quantite'] * data['Prix_unitaire'], 2)
+data["Montant"]: pd.Series = np.round(data["Quantite"] * data["Prix_unitaire"], 2)
 
-mean: float = data['Montant'].mean()
-max_montant: float = data['Montant'].max()
-min_montant: float = data['Montant'].min()
+st.write("Données")
+st.write(data)
+
+# convertir la date en datetime
+data["Date"]: pd.Series = pd.to_datetime(data["Date"])
+
+mean: float = data["Montant"].mean()
+max_montant: float = data["Montant"].max()
+min_montant: float = data["Montant"].min()
 
 # nombre de ventes par produit
-ventes_par_produit: pd.DataFrame = data.groupby('Produit')['Quantite'].sum()
+ventes_par_produit: pd.DataFrame = (
+    data.groupby("Produit")["Quantite"].sum().rename("Ventes")
+)
 print(ventes_par_produit)
+ventes_par_produit.to_csv("data/ventes_par_produit.csv")
 
-
-
-
-
-
+# ventes par mois
+ventes_par_mois: pd.DataFrame = (
+    data.groupby(data["Date"].dt.month_name(locale="fr_FR.utf8"))["Quantite"]
+    .sum()
+    .rename("Ventes")
+    .rename_axis("Mois")
+)
+print(ventes_par_mois)
+ventes_par_mois.to_csv("data/ventes_par_mois.csv")
